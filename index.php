39a4d83330d2346cbe830a83d3b0b7509d18d8a2
Dylan Nirvana <dylan@dylannirvana.com>
2014-06-06 17:11:01 -0400

    installed bootstrap template

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Are you unhitched?</title>
+
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <h1>Cripes!</h1>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscripts.js"></script>
+  </body>
+</html>
