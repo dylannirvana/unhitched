c2a7a73f83550f49dd128ccb5d444cf4c16cc8f2
Dylan Nirvana <dylan@dylannirvana.com>
2014-06-06 22:24:27 -0400

    basic scaffold for website

--- beingSafe.php
+++ beingSafe.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Are you unhitched?</title>
+    <title>Being safe</title>
     <link href='http://fonts.googleapis.com/css?family=Poiret+One|Raleway' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/mystyles.css" rel="stylesheet">
 
   </head>
-  <body id="home">
+  <body id="safe">
     <section class="container">
         <div class="content row">
             <section class="main col col-lg-8">
