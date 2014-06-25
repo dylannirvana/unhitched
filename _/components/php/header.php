7aa8aab2cc6020dbb86f0235d3f7c4c8ffb461ab
Dylan Nirvana <dylan@dylannirvana.com>
2014-06-25 14:14:07 -0400

    draft with collapse menu hamburger

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -8,22 +8,32 @@
         </div> <!-- 2 column -->
         
         <div class="col-lg-7 col-md-8">
-            <nav class="navbar navbar-default navbar-left">
-                <ul class="nav navbar-nav">
-                    <li><a href="startingOver.php">Starting over</a></li> <!-- 1 -->
-                    <li><a href="theRealYou.php">The real you</a></li> <!-- 2 -->
-                    <li><a href="gettingOut.php">Getting out</a></li> <!-- 3 -->                
-                    <li><a href="beingSafe.php">Staying safe</a></li> <!-- 4 -->                
-                    <li class="dropdown"> <!-- 5 -->
-                        <a href="#" class="dropdown-toggle">Contributors</a>
-                        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-                            <li><a tabindex="-1" href="contributors.php">Author</a></li>
-                            <li><a tabindex="-1" href="contributors.php">Author</a></li>
-                            <li><a tabindex="-1" href="contributors.php">About us</a></li>
-                        </ul> <!-- dropdown-menu -->
-                    </li> <!-- dropdown -->
-                    <li><a href="engage.php">Engage</a></li> <!-- 6 -->
-                </ul> <!-- ul navbar -->
+            <nav class="navbar navbar-default navbar-left" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" 
+                    data-toggle="collapse" data-target="#collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="startingOver.php">Starting over</a></li> <!-- 1 -->
+                        <li><a href="theRealYou.php">The real you</a></li> <!-- 2 -->
+                        <li><a href="gettingOut.php">Getting out</a></li> <!-- 3 -->                
+                        <li><a href="beingSafe.php">Staying safe</a></li> <!-- 4 -->                
+                        <li class="dropdown"> <!-- 5 -->
+                            <a href="#" class="dropdown-toggle">Contributors</a>
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+                                <li><a tabindex="-1" href="contributors.php">Author</a></li>
+                                <li><a tabindex="-1" href="contributors.php">Author</a></li>
+                                <li><a tabindex="-1" href="contributors.php">About us</a></li>
+                            </ul> <!-- dropdown-menu -->
+                        </li> <!-- dropdown -->
+                        <li><a href="engage.php">Engage</a></li> <!-- 6 -->
+                    </ul> <!-- ul navbar -->
+                </div>
             </nav> <!-- navbar -->
                         
             <section class="row">
